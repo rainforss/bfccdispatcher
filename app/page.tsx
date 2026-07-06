8b153014834084e7ae3813b06d00d474eb295216
rainforss <shengyan@ualberta.ca>
2026-07-06 16:54:11 -0600

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+export default function Home() {
+  const urlQuery = new URLSearchParams(window.location.search);
+  const ani = urlQuery.get("ani");
+  const dnis = urlQuery.get("dnis");
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            BFCC Dispatcher
+          </h1>
+          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+            Customer Phone Number: {ani || "Not Provided"}
+          </p>
+          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+            Dialed Phone Number: {dnis || "Not Provided"}
+          </p>
+          <p>
+            {urlQuery.toString() === "" ? (
+              <span className="text-red-500">
+                No query parameters provided.
+              </span>
+            ) : (
+              <span className="text-green-500">{urlQuery.toString()}</span>
+            )}
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+}
